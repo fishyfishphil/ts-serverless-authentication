8be4fe6992b84c49fb9d68930f4167e62563d92b
SansDeus <33769513+SansDeus@users.noreply.github.com>
2020-08-28 12:58:22 -0500

    Cleanup
    Tab indentation. What tabs are for. Updated package.json with compile with tsc.

--- src/profile.ts
+++ src/profile.ts
@@ -3,59 +3,59 @@
  */
 
 function formatAddress(address: { formatted?: string, street_address?: string, postal_code: string, locality?: string, country?: string}) {
-  const result = address;
-  if (result) {
-    result.formatted =
-      `${result.street_address}\n${result.postal_code} ${result.locality}\n${result.country}`;
-    return result;
-  }
-  return null;
+	const result = address;
+	if (result) {
+		result.formatted =
+			`${result.street_address}\n${result.postal_code} ${result.locality}\n${result.country}`;
+		return result;
+	}
+	return null;
 }
 
 export class Profile {
-  _raw: any;
-  [key: string]: any;
-  /**
-   * @param data {object}
-   */
-  
-  constructor(data: any) {
-    const fields = [
-      '_raw',
-      'address',
-      'at_hash',
-      'birthdate',
-      'email_verified',
-      'email',
-      'family_name',
-      'gender',
-      'given_name',
-      'id',
-      'locale',
-      'middle_name',
-      'name',
-      'nickname',
-      'phone_number_verified',
-      'phone_number',
-      'picture',
-      'preferred_username',
-      'profile',
-      'provider',
-      'sub',
-      'updated_at',
-      'website',
-      'zoneinfo'
-    ]
-    this._raw = data; // eslint-disable-line no-underscore-dangle
-    fields.forEach((field) => {
-      if (Object.hasOwnProperty.call(data, field)) {
-        const value = data[field]
-        if (field === 'address') {
-          this.address = formatAddress(data.address)
-        } else {
-          this[field] = value || null
-        }
-      }
-    })
-  }
+	_raw: any;
+	[key: string]: any;
+	/**
+	 * @param data {object}
+	 */
+	
+	constructor(data: any) {
+		const fields = [
+			'_raw',
+			'address',
+			'at_hash',
+			'birthdate',
+			'email_verified',
+			'email',
+			'family_name',
+			'gender',
+			'given_name',
+			'id',
+			'locale',
+			'middle_name',
+			'name',
+			'nickname',
+			'phone_number_verified',
+			'phone_number',
+			'picture',
+			'preferred_username',
+			'profile',
+			'provider',
+			'sub',
+			'updated_at',
+			'website',
+			'zoneinfo'
+		]
+		this._raw = data; // eslint-disable-line no-underscore-dangle
+		fields.forEach((field) => {
+			if (Object.hasOwnProperty.call(data, field)) {
+				const value = data[field]
+				if (field === 'address') {
+					this.address = formatAddress(data.address)
+				} else {
+					this[field] = value || null
+				}
+			}
+		})
+	}
 }
